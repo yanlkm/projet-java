ae090e9ce47355069cad3b76a2135bfc0211cd89
glenn louis <glenn.louis@etudiant.univ-brest.fr>
2022-11-24 15:34:28 +0100

    ajout TestMembre

--- Projet MLL/src/association/Membre.java
+++ Projet MLL/src/association/Membre.java
@@ -109,5 +109,37 @@ public class Membre implements InterMembre {
       return null;
     }
   }
+
+  public String getNom() {
+    return nom;
+  }
+
+  public void setNom(String nom) {
+    this.nom = nom;
+  }
+
+  public String getPrenom() {
+    return prenom;
+  }
+
+  public void setPrenom(String prenom) {
+    this.prenom = prenom;
+  }
+
+  public String getAdresse() {
+    return adresse;
+  }
+
+  public void setAdresse(String adresse) {
+    this.adresse = adresse;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
   
 }
