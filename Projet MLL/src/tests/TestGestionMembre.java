08a11f9283d4ab7ec33f0a3dd0bd115c7f24e674
Yan LIKEME <yanmarcelferel.likeme@etudiant.univ-brest.fr>
2022-11-25 18:05:31 +0100

    commit boss

--- Projet MLL/src/tests/TestGestionMembre.java
+++ Projet MLL/src/tests/TestGestionMembre.java
@@ -4,109 +4,183 @@ package tests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.HashSet;
+import java.util.Set;
 import association.GestionMembre;
+import association.InterMembre;
+import association.Membre;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-  public class TestGestionMembre {
-	
-	
 
-   /**
+/**
+*Definition d'une classe implémentant l'interface IntergestionMembre qui permet de gerer
+* un membre dans une association.
+
+ * @author yanmarcelferel.l
+
+ *
+ */
+public class TestGestionMembre {
+
+  /**
    * Une information complï¿½te : prï¿½nom, nom, adresse et age.
-	   */
-	  
-    private GestionMembre gerer;
-	  
-	  /**
-	   * Instancie une information basique et une complï¿½te pour les tests.
-	   *
-	   * @throws Exception ne peut pas ï¿½tre levï¿½e ici
-	   */ 
-	  @BeforeEach
-	  void setUp() throws Exception {
-	    gerer = new GestionMembre();
-	    
-	  }
-	  
-	  /**
-	   * Ne fait rien aprï¿½s les tests : ï¿½ modifier au besoin.
-	   *
-	   * @throws Exception ne peut pas ï¿½tre levï¿½e ici
-	   */
-	  @AfterEach
-	  void tearDown() throws Exception {}
-	  
-	  /**
-	   * Vï¿½rifie que l'on peut positionner un age de 25 ans.
-	   */
-	  @Test
-	  void testAge25Basique() {
-	    infoBasique.setAge(25);
-	    assertEquals(infoBasique.getAge(), 25);
-	  }
-	  
-	  /**
-	   * Vï¿½rifie qu'on ne peut pas positionner un age nï¿½gatif sur une information
-	   * basique.
-	   */
-	  @Test
-	  void testAgeNegatifBasique() {
-	    infoBasique.setAge(-20);
-	    assertTrue(infoBasique.getAge() != -20);
-	  }
-	  
-	  /**
-	   * Vï¿½rifie qu'on ne peut pas positionner un age nï¿½gatif sur une information
-	   * complï¿½te.
-	   */
-	  @Test
-	  void testAgeNegatifComplet() {
-	    int age = infoComplete.getAge();
-	    infoComplete.setAge(-20);
-	    assertEquals(infoComplete.getAge(), age);
-	  }
-	  
-	  /**
-	   * Vï¿½rifie qu'une adresse n'est pas null quand on crï¿½e une information
-	   * personnelle.
-	   */
-	  @Test
-	  void testAdresseNonNull() {
-	    assertTrue(infoBasique.getAdresse() != null);
-	    assertTrue(infoComplete.getAdresse() != null);
-	  }
-	  
-	  /**
-	   * Vï¿½rifie qu'on ne peut pas positionner une adresse null sur une information
-	   * existante.
-	   */
-	  @Test
-	  void testSetterAdresseNull() {
-	    infoComplete.setAdresse(null);
-	    assertTrue(infoComplete.getAdresse() != null);
-	  }
-	  
-	  /**
-	   * Vï¿½rifie que les paramï¿½tres des constructeurs sont correctement gï¿½rï¿½s.
-	   */
-	  @Test
-	  void testConstructeur() {
-	    InformationPersonnelle inf =
-	        new InformationPersonnelle("Vador", "Dark", null, -30);
-	    assertEquals(inf.getNom(), "Vador");
-	    assertEquals(inf.getPrenom(), "Dark");
-	    assertTrue(inf.getAdresse() != null);
-	    assertTrue(inf.getAge() >= 0);
-	  }
+   */
 
-	public GestionMembre getGerer() {
-		return gerer;
-	}
+  private GestionMembre gerer;
+  
+  /**
+   * Instancie une information basique et une complï¿½te pour les tests.
+   *
+   * @throws Exception ne peut pas ï¿½tre levï¿½e ici
+   */ 
+  @BeforeEach
+  void setUp() throws Exception {
+    gerer = new GestionMembre();
+    
+  }
+  
+  /**
+   * Ne fait rien aprï¿½s les tests : ï¿½ modifier au besoin.
+   *
+   * @throws Exception ne peut pas ï¿½tre levï¿½e ici
+   */
+  @AfterEach
+  void tearDown() throws Exception {}
+  
+  /**
+   * Vérifie qu'on peut avoir une liste de membres à un évenement  qui est vide.
+   */
+  
+  @Test
+  void testvidelistemembres() {
+    Set<InterMembre> test1 = new HashSet<InterMembre>();
+ 
+    gerer.setMembres(test1);
+  
+    assertEquals(gerer.getMembres(), test1);
+  }
+  
+  /**
+   * Vï¿½rifie qu'on ne peut pas positionner un age nï¿½gatif sur une information
+   * basique.
+   */
 
-	public void setGerer(GestionMembre gerer) {
-		this.gerer = gerer;
-	}
+  @Test
+  void testajoutunmembre() {
+    Set<InterMembre> test2 = gerer.getMembres();
+    InterMembre type1 =  new Membre("Skywalker", "Luke", "UBO", 23);
+    gerer.ajouterMembre(type1);
+
+    assertEquals(gerer.getMembres(), test2);
+  }
+  
+  /**
+   * Vï¿½rifie qu'on ne peut pas positionner un age nï¿½gatif sur une information
+   * complï¿½te.
+   */
+  @Test
+  void testsupprimermembre() {
+
+    Set<InterMembre> test2 = gerer.getMembres();
+    InterMembre type1 =  new Membre("Skywalker", "Luke", "UBO", 23);
+    gerer.ajouterMembre(type1);
+    gerer.supprimerMembre(type1);
+   
+    assertEquals(gerer.getMembres(), test2);
+  }
+ 
+  @Test
+  void testrenvoiliste() {
+
+    Set<InterMembre> test2 = new HashSet<InterMembre>();
+    InterMembre type1 =  new Membre("Skywalker", "Luke", "UBO", 23);
+    InterMembre type2 =  new Membre("Sar", "Lucie", "UBO", 24);
+    InterMembre type3 =  new Membre("Kywa", "Aminata", "UBO", 29);
+    test2.add(type1);
+    test2.add(type2);
+    test2.add(type3);
+    gerer.ajouterMembre(type1);
+    gerer.ajouterMembre(type2);   
+    gerer.ajouterMembre(type3);
+
+    
+    assertEquals(gerer.ensembleMembres(), test2);
+  }
+  
+ 
+  @Test
+  void testverifierpresident() {
+
+    InterMembre type1 =  new Membre("Skywalker", "Luke", "UBO", 23);
+    InterMembre type2 =  new Membre("Sar", "Lucie", "UBO", 24);
+    gerer.ajouterMembre(type1);
+    gerer.ajouterMembre(type2);
+    gerer.setPresident(type2);
+    assertEquals(gerer.president(), type2);
+  }
+
+  @Test
+  void testverifierpaspresident() {
+
+    InterMembre type1 =  new Membre("Skywalker", "Luke", "UBO", 23);
+    InterMembre type2 =  new Membre("Sar", "Lucie", "UBO", 24);
+    gerer.ajouterMembre(type1);
+    
+    gerer.setPresident(type1);
+    assertTrue(gerer.president() != type2);
+  }
+  
+
+  /*  @Test
+  void testequal() {
+
+    InterMembre type1 =  new Membre("Sar", "Lucie", "UBO", 24);
+    InterMembre type2 =  new Membre("Sar", "Lucie", "UBO", 24);
+    gerer.ajouterMembre(type1);
+    assertTrue(gerer.equals(type2));
+  }*/
+
+  /**
+   * Vï¿½rifie qu'une adresse n'est pas null quand on crï¿½e une information
+   * personnelle.
+   
+  @Test
+  void testAdresseNonNull() {
+    assertTrue(infoBasique.getAdresse() != null);
+    assertTrue(infoComplete.getAdresse() != null);
+  }
+  
+  /**
+   * Vï¿½rifie qu'on ne peut pas positionner une adresse null sur une information
+   * existante.
+   
+  @Test
+  void testSetterAdresseNull() {
+    infoComplete.setAdresse(null);
+    assertTrue(infoComplete.getAdresse() != null);
+  }
+  
+  /**
+   * Vï¿½rifie que les paramï¿½tres des constructeurs sont correctement gï¿½rï¿½s.
+  
+  @Test
+  void testConstructeur() {
+    InformationPersonnelle inf =
+        new InformationPersonnelle("Vador", "Dark", null, -30);
+    assertEquals(inf.getNom(), "Vador");
+    assertEquals(inf.getPrenom(), "Dark");
+    assertTrue(inf.getAdresse() != null);
+    assertTrue(inf.getAge() >= 0);
+  }
+
+public GestionMembre getGerer() {
+	return gerer;
+}
+
+public void setGerer(GestionMembre gerer) {
+	this.gerer = gerer;
+}*/
   
 }
